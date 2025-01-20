b26edd6101433b01897e78cbdbfd0a357f9f81f5
fthx <23138504+fthx@users.noreply.github.com>
2025-01-20 15:41:27 +0100

    Add files via upload

---
+++ extension.js
@@ -0,0 +1,29 @@
+//    Move date to the right
+//    GNOME Shell extension
+//    @fthx 2025
+
+
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+
+export default class MoveDateExtension {
+    _moveDate(active) {
+        if (active) {
+            Main.sessionMode.panel.center = Main.sessionMode.panel.center.filter(item => item != 'dateMenu')
+            Main.sessionMode.panel.right.splice(-1, 0, 'dateMenu');
+        } else {
+            Main.sessionMode.panel.right = Main.sessionMode.panel.right.filter(item => item != 'dateMenu')
+            Main.sessionMode.panel.center.push('dateMenu');
+        }
+
+        Main.panel._updatePanel();
+    }
+
+    enable() {
+        this._moveDate(true);
+    }
+
+    disable() {
+        this._moveDate(false);
+    }
+}
